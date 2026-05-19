561ef61eb6a476c9c266b55bf489759396d01c4f
PetraAnchor <fearofegoism@gmail.com>
2026-05-19 20:46:31 +0700

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,14 @@
+local Scripts = {
+    [2809202155] = "https://raw.githubusercontent.com/crownedwithbloodytears/SimforeaHub/refs/heads/main/Yba%20Main.lua",
+    [3978370137] = "https://raw.githubusercontent.com/crownedwithbloodytears/SimforeaHub/refs/heads/main/GPO.lua",
+    [11424731604] = "https://raw.githubusercontent.com/crownedwithbloodytears/SimforeaHub/refs/heads/main/Cupid%20Dungeon.lua"
+}
+
+local PlaceId = game.PlaceId
+local ScriptUrl = Scripts[PlaceId]
+
+if ScriptUrl then
+    loadstring(game:HttpGet(ScriptUrl))()
+else
+    warn("Скрипт для этого плейса не найден. PlaceId: " .. tostring(PlaceId))
+end
