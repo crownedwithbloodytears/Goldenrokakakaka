bf7c93ecc3ddfc0b5e23ddb47755dadff50794bb
PetraAnchor <fearofegoism@gmail.com>
2026-05-30 22:33:33 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,7 +1,8 @@
 local Scripts = {
     [2809202155] = "https://raw.githubusercontent.com/crownedwithbloodytears/SimforeaHub/refs/heads/main/Yba%20Main.lua",
     [3978370137] = "https://raw.githubusercontent.com/crownedwithbloodytears/SimforeaHub/refs/heads/main/GPO.lua",
-    [11424731604] = "https://raw.githubusercontent.com/crownedwithbloodytears/SimforeaHub/refs/heads/main/Cupid%20Dungeon.lua"
+    [11424731604] = "https://raw.githubusercontent.com/crownedwithbloodytears/SimforeaHub/refs/heads/main/Cupid%20Dungeon.lua",
+    [445664957] = "https://raw.githubusercontent.com/crownedwithbloodytears/SimforeaHub/refs/heads/main/Parkour%20legacy%20bags%20esp.lua"
 }
 
 local PlaceId = game.PlaceId
